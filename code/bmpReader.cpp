6652bf65c015adaa03113abbac40c73a11c08187
Juan <jlq2@alu.ua.es>
2017-09-26 10:32:59 +0200

    fix a small error

--- code/bmpReader.cpp
+++ code/bmpReader.cpp
@@ -35,32 +35,36 @@ RGBApixel bilinealInterpolation(RGBApixel pixela1, RGBApixel pixela2, RGBApixel 
 
 
     
-    int newRed = pixela1.Red * (1 - x) * ( 1 - y) +
+    float newRed = pixela1.Red * (1 - x) * ( 1 - y) +
             pixelb1.Red * (x) * (1 - y) +
             pixela2.Red * (1-x) * (y) +
             pixelb2.Red * x * y;
 
-    int newGreen = pixela1.Green * (1 - y) * ( 1 - y) +
+    float newGreen = pixela1.Green * (1 - y) * ( 1 - y) +
             pixelb1.Green * (x) * (1 - y) +
             pixela2.Green * (1-x) * (y) +
             pixelb2.Green * x * y;
 
-    int newBlue = pixela1.Blue * (1 - y) * ( 1 - y) +
+    float newBlue = pixela1.Blue * (1 - y) * ( 1 - y) +
             pixelb1.Blue * (x) * (1 - y) +
             pixela2.Blue * (1-x) * (y) +
             pixelb2.Blue * x * y;
 
 
-    int newAlpha = pixela1.Alpha * (1 - y) * ( 1 - y) +
+    float newAlpha = pixela1.Alpha * (1 - y) * ( 1 - y) +
             pixelb1.Alpha * (x) * (1 - y) +
             pixela2.Alpha * (1-x) * (y) +
             pixelb2.Alpha * x * y;
             
             
     tmp.Red = newRed;
+
+
     tmp.Green = newGreen;
     tmp.Blue = newBlue;
     tmp.Alpha = newAlpha;
+
+
     return tmp;
 
 }
@@ -109,11 +113,12 @@ int main(int argc, char* argv[]){
       long double ini = tiempo();
 
 
-
+    //Bucles para recorrer todo los pixeles
 	for(int i=0; i< width ; i++){
 
     		for(int j=0; j < height; j++){
 
+                //
     			if(j%2 == 1){
                     if(i != 0 && j != 0 && i < width - 1  && j < height -1){
                             RGBApixel pixel1 = OriginalImage.GetPixel(i/2,j/2);
@@ -121,7 +126,7 @@ int main(int argc, char* argv[]){
                             RGBApixel pixel3 = OriginalImage.GetPixel(i/2,(j+1)/2);
                             RGBApixel pixel4 = OriginalImage.GetPixel((i+1)/2,(j+1)/2);
 
-                            RGBApixel tmp = bilinealInterpolation( pixel1,pixel2,pixel3,pixel4,0.5,0.75 );  
+                            RGBApixel tmp = bilinealInterpolation( pixel1,pixel2,pixel3,pixel4,0.45,0.45 );  
                             
                             FinalImage.SetPixel(i,j,tmp); 
 
@@ -133,7 +138,7 @@ int main(int argc, char* argv[]){
                             RGBApixel pixel3 = OriginalImage.GetPixel(i/2,(j+1)/2);
                             RGBApixel pixel4 = OriginalImage.GetPixel((i+1)/2,(j+1)/2);
 
-                            RGBApixel tmp = bilinealInterpolation( pixel1,pixel2,pixel3,pixel4,0.25,0.5 );  
+                            RGBApixel tmp = bilinealInterpolation( pixel1,pixel2,pixel3,pixel4,0.55,0.55 );  
                             
                             FinalImage.SetPixel(i,j,tmp);                 
                     }
